cce05ef248026cd819664fb6b2f711de902ea1c0
xiedongxin <xiedongxin@cmos.chinamobile.com>
2017-11-04 16:05:22 +0800

    添加日志

--- edccommon-service-impl/src/main/java/com/cmos/edccommon/ServiceApplication.java
+++ edccommon-service-impl/src/main/java/com/cmos/edccommon/ServiceApplication.java
@@ -10,12 +10,14 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import com.cmos.common.spring.ApplicationStarter;
+import com.cmos.core.logger.interceptor.EnableLog4xComponent;
 
 @Configuration
 @SpringBootApplication
 @EnableAspectJAutoProxy
 @EnableAutoConfiguration
 @EnableDataSource
+@EnableLog4xComponent
 @ComponentScan(basePackages = {"com.cmos.edccommon"})
 public class ServiceApplication {
 
--- edccommon-web/src/main/java/com/cmos/edccommon/web/WebApplication.java
+++ edccommon-web/src/main/java/com/cmos/edccommon/web/WebApplication.java
@@ -2,6 +2,8 @@ package com.cmos.edccommon.web;
 
 import com.cmos.common.exception.autoconfig.EnableGeneralException;
 import com.cmos.common.web.config.DefaultWebMvcConfig;
+import com.cmos.core.logger.interceptor.EnableLog4xComponent;
+
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -16,6 +18,7 @@ import com.cmos.common.spring.ApplicationStarter;
 @EnableAspectJAutoProxy
 @EnableAutoConfiguration
 @EnableGeneralException
+@EnableLog4xComponent
 @ComponentScan(basePackages = {"com.cmos.edccommon","com.cmos.cache"})
 @Import(DefaultWebMvcConfig.class)
 public class WebApplication {
